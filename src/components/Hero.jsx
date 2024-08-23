435df9321599f15fdba34877009d1e8333c38cbe
diduladishan <diduladishan14@gmail.com>
2024-08-23 15:15:31 +0530

    collaga added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
-import Hero from "../assets/hero.jpg";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+import Hero from "../assets/hero.jpg"
+import Footer from "./Footer"
+import Navbar from "./Navbar"
+
 const hero = () => {
   return (
     <div className="container mx-auto">
@@ -9,37 +10,37 @@ const hero = () => {
         <div>
           {" "}
           <Navbar />
-          <div className="flex flex-col items-center mt-6 lg:mt-10">
-            <h1 className="text-4xl tracking-wide text-center sm:text-6xl lg:text-7xl">
+          <div className="mt-6 flex flex-col items-center lg:mt-10">
+            <h1 className="text-center text-4xl tracking-wide sm:text-6xl lg:text-7xl">
               Create more
-              <span className="bg-gradient-to-r from-[#D42680] to-[#3D3BC9] text-transparent bg-clip-text font-bold">
+              <span className="bg-gradient-to-r from-[#D42680] to-[#3D3BC9] bg-clip-text font-bold text-transparent">
                 {" "}
                 MEME
               </span>{" "}
               in less time!
             </h1>
-            <p className="max-w-4xl mt-10 text-lg text-center text-neutral-500">
+            <p className="mt-10 max-w-4xl text-center text-lg text-neutral-500">
               Create and share memes instantly with MemeMaster! Choose from
               templates or upload your own images. Perfect for beginners and
               pros. Start creating and join the meme revolution!
               <footer className="mt-10 border-t border-neutral-700"></footer>
             </p>
-            <div className="flex flex-col items-center justify-center my-10 sm:flex-row">
+            <div className="my-10 flex flex-col items-center justify-center sm:flex-row">
               <a
                 href="#"
-                className="bg-gradient-to-r from-[#D42680] to-[#3D3BC9] py-2 px-6 mx-2 sm:mx-3 rounded-md text-xs sm:text-sm"
+                className="mx-2 rounded-md bg-gradient-to-r from-[#D42680] to-[#3D3BC9] px-6 py-2 text-xs sm:mx-3 sm:text-sm"
               >
                 Connect Wallet
               </a>
               <a
                 href="#"
-                className="px-6 py-2 mt-4 text-xs text-center border rounded-md sm:mt-0 sm:text-sm"
+                className="mt-4 rounded-md border px-6 py-2 text-center text-xs sm:mt-0 sm:text-sm"
               >
                 Documentation
               </a>
             </div>
 
-            <div className="flex mt-10 justify-center bg-gradient-to-r from-[#D42680] to-[#3D3BC9]">
+            <div className="mt-10 flex justify-center bg-gradient-to-r from-[#D42680] to-[#3D3BC9]">
               <img
                 className=" w-1.2 mx-2 my-4"
                 src={Hero}
@@ -51,7 +52,7 @@ const hero = () => {
       </div>
       <Footer />
     </div>
-  );
-};
+  )
+}
 
-export default hero;
+export default hero
