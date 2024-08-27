6075e9af58d153e033a380cb06bbdc0a0ff37bb5
diduladishan <diduladishan14@gmail.com>
2024-08-27 15:40:08 +0530

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ import { resourcesLinks, platformLinks, communityLinks } from "../constants1"
 const Footer = () => {
   return (
     <footer className="mx-auto mt-20 flex w-[100%] max-w-screen-xl flex-col items-center justify-center border-t border-neutral-700 py-10">
-      <div className="flex cursor-pointer">
+      <div className="flex cursor-pointer items-center justify-center">
         <img
           className="h-6 w-6 sm:h-8 sm:w-8 md:h-10 md:w-10"
           src={Logo}
@@ -17,7 +17,7 @@ const Footer = () => {
         />
       </div>
 
-      <div className="mt-6 flex flex-row justify-between sm:w-[50%] md:w-[30%]">
+      <div className="mt-6 flex flex-row justify-between sm:w-[50%] md:w-[30%] 2xl:ml-4">
         <div className="flex cursor-pointer flex-col items-center px-4">
           <div className="flex h-5 w-5 flex-row items-center justify-center sm:h-7 sm:w-7">
             <img className="h-[80%] w-[80%]" src={X} alt="MEME_Generate" />
