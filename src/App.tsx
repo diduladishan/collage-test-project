04bec3b4342a5419f0b19c76da4c0df6e2b208c7
diduladishan <diduladishan14@gmail.com>
2024-08-27 12:29:22 +0530

    fix deployment issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,6 @@
 import Hero from "./components/Hero"
 import Home from "./components/Home/Home"
 import Login from "./components/Login/Login"
-import MemeEditor from "./components/Meme-Generator/MemeEditor"
-import Canvas from "@/components/Canvas/Canvas"
-import MenuBar from "@/components/Menu/MenuBar"
-import EditingPanel from "@/components/Tab/TabPanel"
-import store from "@/redux/store"
-import { Toaster } from "react-hot-toast"
-import { Provider } from "react-redux"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 function App() {
--- src/components/Hero.d.ts
+++
@@ -1,6 +0,0 @@
-// src/components/Hero.d.ts
-declare module "./Hero" {
-  import React from "react"
-  const Hero: React.FC
-  export default Hero
-}
