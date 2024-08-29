e55aefff5f90a04195abaf6c6bc42f36102b44b0
diduladishan <diduladishan14@gmail.com>
2024-08-29 15:24:10 +0530

    refactor image selector

--- src/components/Meme-Generator/ImageSelector.jsx
+++ src/components/Meme-Generator/ImageSelector.jsx
@@ -1,17 +1,15 @@
-import React, { useRef } from "react";
-
-import image1 from "../../assets/image01.png";
-import image2 from "../../assets/image02.png";
-import image3 from "../../assets/image03.png";
-import image4 from "../../assets/image04.png";
-import image5 from "../../assets/image05.png";
-import image6 from "../../assets/image06.png";
-import image7 from "../../assets/image07.png";
-import image8 from "../../assets/image08.png";
-import image9 from "../../assets/image09.png";
-import image10 from "../../assets/image01.png";
-
-import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
+import image1 from "../../assets/image01.png"
+import image10 from "../../assets/image01.png"
+import image2 from "../../assets/image02.png"
+import image3 from "../../assets/image03.png"
+import image4 from "../../assets/image04.png"
+import image5 from "../../assets/image05.png"
+import image6 from "../../assets/image06.png"
+import image7 from "../../assets/image07.png"
+import image8 from "../../assets/image08.png"
+import image9 from "../../assets/image09.png"
+import React, { useRef } from "react"
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa6"
 
 const images = [
   image1,
@@ -24,40 +22,40 @@ const images = [
   image8,
   image9,
   image10,
-];
+]
 
 const ImageSelector = ({ onImageSelect }) => {
-  const carouselRef = useRef(null);
+  const carouselRef = useRef(null)
 
   const handleNext = () => {
     if (carouselRef.current) {
-      const scrollAmount = carouselRef.current.clientWidth / 2;
-      carouselRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
+      const scrollAmount = carouselRef.current.clientWidth / 2
+      carouselRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" })
     }
-  };
+  }
 
   const handlePrev = () => {
     if (carouselRef.current) {
-      const scrollAmount = carouselRef.current.clientWidth / 2;
-      carouselRef.current.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+      const scrollAmount = carouselRef.current.clientWidth / 2
+      carouselRef.current.scrollBy({ left: -scrollAmount, behavior: "smooth" })
     }
-  };
+  }
 
   const handleSelect = (image) => {
-    onImageSelect(image);
-  };
+    onImageSelect(image)
+  }
 
   return (
     <div className="relative w-full overflow-hidden">
       <button
         onClick={handlePrev}
-        className="absolute top-1/2 left-0 transform -translate-y-1/2 z-10 bg-gray-800 text-white rounded-full size-7 flex items-center justify-center text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl cursor-pointer md:size-8 lg:size-9 xl:size-10 2xl:size-12"
+        className="size-7 md:size-8 lg:size-9 xl:size-10 2xl:size-12 absolute left-0 top-1/2 z-10 flex -translate-y-1/2 transform cursor-pointer items-center justify-center rounded-full bg-gray-800 text-sm text-white md:text-base lg:text-lg xl:text-xl 2xl:text-2xl"
       >
         <FaAngleLeft />
       </button>
       <div
         ref={carouselRef}
-        className="flex overflow-hidden w-full no-scrollbar"
+        className="no-scrollbar flex w-full overflow-hidden"
       >
         <div className="flex">
           {images.map((image, index) => (
@@ -65,10 +63,7 @@ const ImageSelector = ({ onImageSelect }) => {
               key={index}
               src={image}
               alt={`Meme ${index}`}
-              // className="w-[calc(12.5%-10px)] h-auto flex-shrink-0 cursor-pointer border-4 border-gray-300 rounded-md mr-2.5"
-              // className="w-[calc(19.1%-5px)] h-auto flex-shrink-0 cursor-pointer border-[3px] lg:border-4 border-gray-300 rounded-md mr-2.5 sm:w-[calc(16.2%-6px) md:w-[calc(16.2%-6px)] lg:w-[calc(14%-7px)] xl:w-[calc(12.5%-10px)] 2xl:w-[calc(12.5%-10px)]"
-
-              className="h-auto flex-shrink-0 cursor-pointer border-[3px] lg:border-4 border-gray-300 rounded-md mr-2.5 w-[calc(33%-6px)] sm:w-[calc(33%-6px)] md:w-[calc(16.2%-6px)] lg:w-[calc(14%-7px)] xl:w-[calc(12.5%-10px)] 2xl:w-[calc(12.5%-10px)]"
+              className="mr-2.5 h-auto w-[calc(33%-6px)] flex-shrink-0 cursor-pointer rounded-md border-[3px] border-gray-300 sm:w-[calc(33%-6px)] md:w-[calc(16.2%-6px)] lg:w-[calc(14%-7px)] lg:border-4 xl:w-[calc(12.5%-10px)] 2xl:w-[calc(12.5%-10px)]"
               onClick={() => handleSelect(image)}
             />
           ))}
@@ -76,12 +71,12 @@ const ImageSelector = ({ onImageSelect }) => {
       </div>
       <button
         onClick={handleNext}
-        className="absolute top-1/2 right-0 transform -translate-y-1/2 z-10 bg-gray-800 text-white rounded-full size-7 flex items-center justify-center text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl cursor-pointer md:size-8 lg:size-9 xl:size-10 2xl:size-12"
+        className="size-7 md:size-8 lg:size-9 xl:size-10 2xl:size-12 absolute right-0 top-1/2 z-10 flex -translate-y-1/2 transform cursor-pointer items-center justify-center rounded-full bg-gray-800 text-sm text-white md:text-base lg:text-lg xl:text-xl 2xl:text-2xl"
       >
         <FaAngleRight />
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default ImageSelector;
+export default ImageSelector
