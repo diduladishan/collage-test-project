7177a0f48c18a875c5293e0be0fd77fad33c040c
diduladishan <diduladishan14@gmail.com>
2024-08-29 17:30:32 +0530

    update meme editor

--- src/components/Meme-Generator/MemeEditor.jsx
+++ src/components/Meme-Generator/MemeEditor.jsx
@@ -398,8 +398,6 @@ const MemeEditor = () => {
                     </p>
                   </div>
                 </div>
-
-                <TemplateControl />
               </div>
             )}
 
