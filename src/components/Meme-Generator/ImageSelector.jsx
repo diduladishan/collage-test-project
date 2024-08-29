bc9c38436baa3efbe5f63b9f9eaeb82b6b5de4fd
diduladishan <diduladishan14@gmail.com>
2024-08-29 16:50:24 +0530

    3 meme templates added

--- src/components/Meme-Generator/ImageSelector.jsx
+++ src/components/Meme-Generator/ImageSelector.jsx
@@ -8,24 +8,32 @@ import image6 from "../../assets/image06.png"
 import image7 from "../../assets/image07.png"
 import image8 from "../../assets/image08.png"
 import image9 from "../../assets/image09.png"
+import image11 from "../../assets/image11.png"
+import image12 from "../../assets/image12.png"
+import image13 from "../../assets/image13.png"
 import React, { useRef } from "react"
 import { FaAngleRight, FaAngleLeft } from "react-icons/fa6"
+import { useNavigate } from "react-router-dom"
 
 const images = [
-  image1,
-  image2,
-  image3,
-  image4,
-  image5,
-  image6,
-  image7,
-  image8,
-  image9,
-  image10,
+  { src: image11, route: "/template1" }, // Adding new image with route
+  { src: image12, route: "/template2" }, // Adding new image with route
+  { src: image13, route: "/template3" }, // Adding new image with route
+  { src: image1, route: null },
+  { src: image2, route: null },
+  { src: image3, route: null },
+  { src: image4, route: null },
+  { src: image5, route: null },
+  { src: image6, route: null },
+  { src: image7, route: null },
+  { src: image8, route: null },
+  { src: image9, route: null },
+  { src: image10, route: null },
 ]
 
 const ImageSelector = ({ onImageSelect }) => {
   const carouselRef = useRef(null)
+  const navigate = useNavigate() // Initialize useNavigate hook
 
   const handleNext = () => {
     if (carouselRef.current) {
@@ -42,7 +50,11 @@ const ImageSelector = ({ onImageSelect }) => {
   }
 
   const handleSelect = (image) => {
-    onImageSelect(image)
+    if (image.route) {
+      navigate(image.route) // Navigate to the route if it exists
+    } else {
+      onImageSelect(image.src) // Call onImageSelect if no route is defined
+    }
   }
 
   return (
@@ -61,7 +73,7 @@ const ImageSelector = ({ onImageSelect }) => {
           {images.map((image, index) => (
             <img
               key={index}
-              src={image}
+              src={image.src}
               alt={`Meme ${index}`}
               className="mr-2.5 h-auto w-[calc(33%-6px)] flex-shrink-0 cursor-pointer rounded-md border-[3px] border-gray-300 sm:w-[calc(33%-6px)] md:w-[calc(16.2%-6px)] lg:w-[calc(14%-7px)] lg:border-4 xl:w-[calc(12.5%-10px)] 2xl:w-[calc(12.5%-10px)]"
               onClick={() => handleSelect(image)}
